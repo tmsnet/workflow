ed08efed772b4bd5871112792f8c377a47560e1e
tmsnet <tmsnet@126.com>
2016-08-17 15:58:27 +0800

    Added Browserify

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var util = require('gulp-util');
 var coffee = require('gulp-coffee');
 var concat = require('gulp-concat');
+var browserify = require('gulp-browserify');
+
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
 var jsSources =[
@@ -20,5 +22,6 @@ gulp.task('coffee',function(){
 gulp.task('js',function(){
     gulp.src(jsSources)
         .pipe(concat('script.js'))
+        .pipe(browserify())
         .pipe(gulp.dest('builds/development/js'))
 })
