de19bae45b34e9bc4174ed09c9587f304acade55
tmsnet <tmsnet@126.com>
2016-08-18 10:12:07 +0800

    Added Static File Monitoring

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,8 @@ var jsSources =[
     'components/scripts/template.js'
 ];
 var sassSources= ['components/sass/style.scss'];
+var htmlSources=['builds/development/*.html'];
+var jsonSources=['builds/development/js/*.json'];
 
 gulp.task('coffee',function(){
     gulp.src(coffeeSources)
@@ -45,6 +47,18 @@ gulp.task('watch',function(){
     gulp.watch(coffeeSources,['coffee']);
     gulp.watch(jsSources,['js']);
     gulp.watch('components/sass/*.scss',['compass']);
+    gulp.watch(htmlSources,['html']);
+    gulp.watch(jsonSources,['json']);
+})
+
+gulp.task('html',function(){
+    gulp.src(htmlSources)
+        .pipe(connect.reload())
+})
+
+gulp.task('json',function(){
+    gulp.src(jsonSources)
+        .pipe(connect.reload())
 })
 
 gulp.task('connect',function(){
@@ -55,4 +69,4 @@ gulp.task('connect',function(){
     });
 })
 
-gulp.task('default',['coffee','js','compass','connect','watch']);
+gulp.task('default',['html','json','coffee','js','compass','connect','watch']);
